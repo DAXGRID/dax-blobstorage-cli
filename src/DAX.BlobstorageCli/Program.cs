6ef29d882d7a8e5155e9f6b9657d92be275a14d5
runeanielsen <runenielsen@runbox.com>
2024-10-10 14:26:18 +0200

    setup default commands for upload and download file

--- src/DAX.BlobstorageCli/Program.cs
+++ src/DAX.BlobstorageCli/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace DAX.BlobstorageCli;
+﻿using System.CommandLine;
+
+namespace DAX.BlobstorageCli;
 
 internal sealed class Program
 {
-    public static void Main()
+   static async Task<int> Main(string[] args)
     {
-        Console.WriteLine("Hello, world!");
+        var fileOption = new Option<bool>(
+            name: "--file",
+            description: "The file to read and display on the console.");
+
+        var rootCommand = new RootCommand("Sample app for System.CommandLine");
+
+        var uploadCommand = new Command("upload", "Upload a file to blobstorage");
+        rootCommand.Add(uploadCommand);
+
+        var downloadCommand = new Command("download", "Download a file from blobstorage");
+        rootCommand.Add(downloadCommand);
+
+        return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
     }
 }
