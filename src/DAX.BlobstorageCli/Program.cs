6ba5464693f9b5d8fd59a02a488d7c98ad527f39
runeanielsen <runenielsen@runbox.com>
2024-10-11 10:02:55 +0200

    setup sub commands for upload and download files from blobstorage

--- src/DAX.BlobstorageCli/Program.cs
+++ src/DAX.BlobstorageCli/Program.cs
@@ -6,17 +6,30 @@ internal sealed class Program
 {
    static async Task<int> Main(string[] args)
     {
-        var fileOption = new Option<bool>(
-            name: "--file",
-            description: "The file to read and display on the console.");
-
         var rootCommand = new RootCommand("Sample app for System.CommandLine");
 
+        var uploadCommandOptions = new Option<bool>(
+            name: "--file",
+            description: "The file to upload.");
+
         var uploadCommand = new Command("upload", "Upload a file to blobstorage");
-        rootCommand.Add(uploadCommand);
+        uploadCommand.Add(uploadCommandOptions);
+
+        var downloadCommandOptions = new Option<string>(
+            name: "--output",
+            description: "The output path where the file should be downloaded to.");
 
         var downloadCommand = new Command("download", "Download a file from blobstorage");
+        downloadCommand.Add(downloadCommandOptions);
+
         rootCommand.Add(downloadCommand);
+        rootCommand.Add(uploadCommand);
+
+        uploadCommand.SetHandler((outputPath) =>
+        {
+            Console.WriteLine($"Output path: {outputPath}");
+        },
+        downloadCommandOptions);
 
         return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
     }
